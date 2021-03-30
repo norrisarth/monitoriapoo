d3242a4ff7914128096594eb097a21b64b3777cf
Vinicius Takeo Friedrich Kuwaki <vtkwki@gmail.com>
2021-03-30 15:57:18 -0300

    aula1-Flutter:esqueleto

---
+++ Flutter/aula1/esqueleto.dart
@@ -0,0 +1,11 @@
+class NOME extends StatefulWidget {
+  @override
+  _NOMEState createState() => _NOMEState();
+}
+
+class _NOMEState extends State<NOME> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
